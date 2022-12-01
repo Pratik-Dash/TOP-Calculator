126c7510fee6bf8cd29ee6d33ab2f5ce31e4a850
Pratik Dash <dashpratik40@gmail.com>
2022-12-01 10:05:46 +0530

    JS: Added delete, clear and add click events

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const container = document.querySelector(".container")
 const buttons = document.querySelectorAll(".button")
 const display = document.querySelector(".display");
+const clearButton = document.querySelector*".clear"
+const DeleteButton = document.querySelector*".Delete"
 for(let button of buttons){
     button.addEventListener("click",(e)=>{
-        if(!button.classList.contains("operator")){
+        if(!button.classList.contains("operator") && !button.classList.contains("equalBtn" && !button.classList.contains("clear") && !button.classList.contains("delete"))){
             display.innerHTML += `<h1>${e.target.value}</h1>`;
         }
         else{
@@ -11,6 +13,7 @@ for(let button of buttons){
         }
     })
 }
+clearButton.addEventListener("click", Clear)
 function Operate(number1, number2,operation){
 
 
@@ -34,4 +37,10 @@ function Divide(a,b){
 
 function Modulo(a,b){
     return a%b;
+}
+
+function Clear(){
+    let currentString = display.innerText;
+    currentString = currentString.slice(currentString.length-1);
+    display.innerHTML = `<h1>${currentString}</h1>`
 }

a5b212a15e232a006c482ca20894bed2c0fd0b5b
Pratik Dash <dashpratik40@gmail.com>
2022-12-03 17:02:53 +0530

    Completed Javascript and CSS

--- app.js
+++ app.js
@@ -1,46 +1,105 @@
-const container = document.querySelector(".container")
-const buttons = document.querySelectorAll(".button")
-const display = document.querySelector(".display");
-const clearButton = document.querySelector*".clear"
-const DeleteButton = document.querySelector*".Delete"
-for(let button of buttons){
-    button.addEventListener("click",(e)=>{
-        if(!button.classList.contains("operator") && !button.classList.contains("equalBtn" && !button.classList.contains("clear") && !button.classList.contains("delete"))){
-            display.innerHTML += `<h1>${e.target.value}</h1>`;
-        }
-        else{
-            display.innerHTML = `<h1>${e.target.value}</h1>`;
-        }
-    })
+let primaryDisplay = document.querySelector('.currentoperanddisplay')
+let secondaryDisplay = document.querySelector('.previousoperanddisplay')
+let currentOperand = '';
+let previousOperand ='';
+let operation = undefined;
+
+document.addEventListener("click",(event)=>{
+
+    if(event.target.matches('.numbutton')){
+
+        appendOperands(event.target.value);
+        setDisplay();
+    }
+    if(event.target.matches('.operator')){
+        initializeOperation(event.target.value)
+       setDisplay();
+    }
+    if(event.target.matches(".equalBtn")){
+        performOperation()
+        setDisplay();
+    }
+    if(event.target.matches('.clear')){
+        allClear();
+        setDisplay();
+    }
+    if(event.target.matches('.delete')){
+        deleteNumbers();
+        setDisplay();
+    }
+
+})
+
+function appendOperands(operand){
+    if(operand === '.' && primaryDisplay.textContent.includes('.')){
+        return;
+    }
+    else{
+        currentOperand = currentOperand.toString()+operand.toString()
+    }
 }
-clearButton.addEventListener("click", Clear)
-function Operate(number1, number2,operation){
+function setDisplay(){
+    
+    primaryDisplay.textContent = `${currentOperand}`
+    if(operation != undefined){
+        secondaryDisplay.textContent = `${previousOperand} ${operation}`
+    }
+    else{
+        secondaryDisplay.textContent = '';
+    }
+}
 
+function initializeOperation(selectedOperation){
+    if(currentOperand === ''){
+        return
+    }
+    if(previousOperand!==''){
+        performOperation();
+    }
+    operation = selectedOperation;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function performOperation(){
+    let result;
+    let previousNumber = parseFloat(previousOperand);
+    let currentNumber = parseFloat(currentOperand)
+    if(isNaN(previousNumber) || isNaN(currentNumber)){
+        return
+    }
+    
+    switch(operation){
+
+        case '+':
+            result = previousNumber + currentNumber;
+            break;
+        case '-':
+            result = previousNumber - currentNumber;
+            break;
+        case 'x':
+            result = previousNumber * currentNumber;
+            break;
+        case '÷':
+            result = previousNumber / currentNumber;
+            break;
+        case '%':
+            result = previousNumber % currentNumber;
+            break;
+        default:
+            return;
+    }
+    currentOperand = result
+    previousOperand = '';
+    operation = undefined;
+}
+function allClear(){
+    currentOperand = '';
+    previousOperand = '';
+    operation = undefined
 
 }
 
-function Add(a,b){
-    return a+b;
-}
-
-function Subtract(a,b){
-return a-b;
-}
-
-function Multiply(a,b){
-    return a*b;
-}
-
-function Divide(a,b){
-    return a/b;
-}
-
-function Modulo(a,b){
-    return a%b;
-}
-
-function Clear(){
-    let currentString = display.innerText;
-    currentString = currentString.slice(currentString.length-1);
-    display.innerHTML = `<h1>${currentString}</h1>`
+function deleteNumbers(){
+    currentOperand = currentOperand.toString().slice(0,-1);
 }
